34b244a250edc793b775cb702ec83c600dc08c50
Murilo Cavalcanti <106565777+CavalcantiMurilo@users.noreply.github.com>
2024-02-19 13:48:11 -0300

    Delete CollectCarBack directory

--- CollectCarBack/src/main/java/com/cars/carapi/model/CarPageDTO.java
+++
@@ -1,8 +0,0 @@
-package com.cars.carapi.model;
-
-import com.cars.carapi.api.response.CarDTO;
-
-import java.util.List;
-
-public record CarPageDTO(List<CarDTO> cars, long elements, int pages ) {
-}
