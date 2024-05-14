619682adaf9f87d104c00ebeeb65eae97eb9e462
Bernardo <bernardo@omotor.com.br>
2024-05-14 16:16:22 -0300

    Cors Config

--- src/main/java/br/com/crud/cars/configurations/CorsConfig.java
+++ src/main/java/br/com/crud/cars/configurations/CorsConfig.java
@@ -1,2 +1,23 @@
-package br.com.crud.cars.configurations;public class CorsConfig {
+package br.com.crud.cars.configurations;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
 }
